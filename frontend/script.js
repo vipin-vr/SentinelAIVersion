2f32d54d3c440d438bbf47354cc5df119102b86d
J Ravi <rravi91001@gmail.com>
2026-08-05 11:04:53 +0530

    Connect frontend to Render backend

--- frontend/script.js
+++ frontend/script.js
@@ -34,7 +34,7 @@ predictBtn.addEventListener("click", async ()=>{
 
     try {
 
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("https://sentinelai-a6aj.onrender.com/predict", {
             method: "POST",
             body: formData
         });
